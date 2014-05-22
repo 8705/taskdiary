9beb9c0e700c7e975c0689dd1d011a8a7eb85886
EgamiYusuke <egpersent@gmail.com>
2014-05-22 21:23:42 +0900

    ロゴにWebフォント適応/カテゴリセレクトのcss微調整

--- views/elements/header.php
+++ views/elements/header.php
@@ -6,6 +6,7 @@
     <title><?php if (isset($title)): echo $this->escape($title) . ' - '; endif; ?>Task Diary</title>
     <link rel="stylesheet" type="text/css" media="screen" href="/css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" media="screen" href="/css/style.css" />
+    <link href='http://fonts.googleapis.com/css?family=Damion' rel='stylesheet' type='text/css'>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
     <script src="/js/taskdiary.js"></script>
     <script src="/js/jquery-ui.js"></script>
