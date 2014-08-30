7a6431368ad7d314d1c9f24c7845cd03a6da4ae7
EgamiYusuke <egpersent@gmail.com>
2014-08-30 23:15:38 +0900

    チェックボックスの有効無効実装

--- views/top/index.php
+++ views/top/index.php
@@ -7,7 +7,7 @@
             <form action="/task/add_task" method="POST">
                 <ul id="task_add">
                     <li class="clearfix">
-                        <input type="text" name="category_name[]" class="input-category" data-input-num="1" placeholder="カテゴリを入力">
+                        <!-- <input type="text" name="category_name[]" class="input-category" data-input-num="1" placeholder="カテゴリを入力"> -->
                         <input type="text" class="input-task form-control" data-input-num="1" name="task_name[]" placeholder="タスクを入力"/>
                     </li>
                 </ul>
@@ -32,13 +32,12 @@
                                 <span class="<?php !is_null($v['task_text'])?print'task-comment':print'task-comment-none';?>">
                                     <a href="javascript:void();" data-task-id="<?php echo $this->escape($v['task_id']); ?>"><span class="glyphicon glyphicon-comment"></span></a>
                                 </span>
-                                <span>
+                                <span class="delete-task">
                                     <a href="/task/delete/<?php echo $this->escape($v['task_id']); ?>"><span class="glyphicon glyphicon-remove-circle"></span></a>
                                 </span>
                                 <?php if($v['task_limit'] <= date('Y-m-d')):?>
                                 <span class="over-deadline glyphicon glyphicon-exclamation-sign"></span>
                                 <?php endif; ?>
-                                <span class="sort-task glyphicon glyphicon-align-justify"></span>
                             </p>
                         </li>
                     <?php endforeach; ?>
@@ -60,16 +59,15 @@
                         <li class="task list-group-item <?php if ($v['task_is_done'] == 1) echo 'done'; ?>" id="task_<?php echo $v['task_id']; ?>">
                             <p>
                                 <input type="hidden" name="<?php echo $v['task_id']; ?>" value="0">
-                                <input type="checkbox" class="check-task" name="<?php echo $v['task_id']; ?>" value="1" <?php if ($v['task_is_done'] == 1) echo "checked='checked'"; ?> >
+                                <input type="checkbox" class="check-task" name="<?php echo $v['task_id']; ?>" value="1" <?php if ($v['task_is_done'] == 1) echo "checked='checked'"; ?> disabled="disabled">
                                 <?php echo $this->escape($v['task_name']); ?>
                                 <?php if (isset($v['category_name'])) echo '('.$this->escape($v['category_name']).')'; ?>
                                 <span class="<?php !is_null($v['task_text'])?print'task-comment':print'task-comment-none';?>">
                                     <a href="javascript:void();" data-task-id="<?php echo $this->escape($v['task_id']); ?>"><span class="glyphicon glyphicon-comment"></span></a>
                                 </span>
-                                <span>
+                                <span class="delete-task">
                                     <a href="/task/delete/<?php echo $this->escape($v['task_id']); ?>"><span class="glyphicon glyphicon-remove-circle"></span></a>
                                 </span>
-                                <span class="sort-task glyphicon glyphicon-align-justify"></span>
                             </p>
                         </li>
                     <?php endforeach; ?>
